5436a3a2746c2dab11a9ef31a2199856fccc753e
tigger <76392809+fronttigger@users.noreply.github.com>
2021-12-17 01:16:49 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+import Message from '@shared/Message'
+
+function IndexPage() {
+  return (
+    <Container>
+      <Message />
+    </Container>
+  )
+}
+
+const Container = styled.div``
+
+export default IndexPage
