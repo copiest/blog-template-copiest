5a3ed304e848f127f6f9d5f47ff9042c38c1c41a
tigger <fronttigger@gmail.com>
2021-12-30 00:57:33 +0900

    fix: mdxComponents props unknown 처리

--- src/components/shared/MDX/Code.tsx
+++ src/components/shared/MDX/Code.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
-const Code = (props: any) => {
+const Code = (props: unknown) => {
   return <SyntaxHighlighter language="javascript" style={vscDarkPlus} {...props} />
 }
 
