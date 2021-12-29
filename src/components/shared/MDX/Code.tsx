48f2a5018e0aacafc81a37c766662aac26eeae33
tigger <fronttigger@gmail.com>
2021-12-30 00:20:17 +0900

    fix: 불필요 컴포넌트 제거

--- src/components/shared/MDX/Code.tsx
+++ src/components/shared/MDX/Code.tsx
@@ -3,10 +3,6 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 const Code = (props: any) => {
-  if (props.language) {
-    return <SyntaxHighlighter language={props.language} style={vscDarkPlus} {...props} />
-  }
-
   return <SyntaxHighlighter language="javascript" style={vscDarkPlus} {...props} />
 }
 
