d848df0c0b9e23fcd2f6f33c4e0669445ae7ea2d
tigger <fronttigger@gmail.com>
2022-01-02 23:19:42 +0900

    feat: recoil 적용 및 스크롤 저장 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,50 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/dist/client/router'
+import { RecoilRoot } from 'recoil'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const Router = useRouter()
+
+  useEffect(() => {
+    window.history.scrollRestoration = 'auto'
+
+    const cachedScrollPositions: Array<[number, number]> = []
+    let shouldScrollRestore: null | { x: number; y: number }
+
+    Router.events.on('routeChangeStart', () => {
+      cachedScrollPositions.push([window.scrollX, window.scrollY])
+    })
+
+    Router.events.on('routeChangeComplete', () => {
+      if (shouldScrollRestore) {
+        const { x, y } = shouldScrollRestore
+        window.scrollTo(x, y)
+        shouldScrollRestore = null
+      }
+      window.history.scrollRestoration = 'auto'
+    })
+
+    Router.beforePopState(() => {
+      if (cachedScrollPositions.length > 0) {
+        const scrolledPosition = cachedScrollPositions.pop()
+        if (scrolledPosition) {
+          shouldScrollRestore = {
+            x: scrolledPosition[0],
+            y: scrolledPosition[1],
+          }
+        }
+      }
+      window.history.scrollRestoration = 'manual'
+      return true
+    })
+  }, [])
+
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
 
 export default App
