7a0a3e252ef420064cd4ea0c6502d535e4cdeb1f
tigger <fronttigger@gmail.com>
2022-01-10 02:18:15 +0900

    feat: 앵커 커스텀 태그 구현

--- src/components/shared/MDX/index.ts
+++ src/components/shared/MDX/index.ts
@@ -2,10 +2,12 @@ import Code from './Code'
 import Paragraph from './Paragraph'
 import Image from './Image'
 import Strong from './Strong'
+import Anchor from './Anchor'
 
 export default {
   code: Code,
   p: Paragraph,
   img: Image,
   strong: Strong,
+  a: Anchor,
 }
