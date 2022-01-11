ce7ad24b26f0e999046e5e32b36f92661a34683f
tigger <fronttigger@gmail.com>
2022-01-12 01:53:02 +0900

    feat: ol, ul 태그 구현

--- src/components/shared/MDX/List/Ol/styles.css.ts
+++ src/components/shared/MDX/List/Ol/styles.css.ts
@@ -1,8 +1,25 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
 
 import { theme } from '#components/shared/theme.css'
 
-export const h1 = style({
-  fontSize: theme.fontSize.xxlarge,
-  fontWeight: theme.fontWeight.bold,
+export const ol = style({
+  marginBottom: '20px',
+  counterReset: 'list-number',
+})
+
+globalStyle(`${ol} > li`, {
+  margin: `${theme.space.medium} 0`,
+  position: 'relative',
+  paddingInlineStart: theme.space.xlarge,
+  color: theme.color.bluegray,
+  fontSize: theme.fontSize.medium,
+})
+
+globalStyle(`${ol} > li:before`, {
+  counterIncrement: 'list-number',
+  content: 'counter(list-number) "."',
+  position: 'absolute',
+  left: 0,
+  fontSize: theme.fontSize.medium,
+  fontWeight: theme.fontWeight.semiBold,
 })
--- src/components/shared/MDX/List/Ul/styles.css.ts
+++ src/components/shared/MDX/List/Ul/styles.css.ts
@@ -1,8 +1,26 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
 
 import { theme } from '#components/shared/theme.css'
 
-export const h1 = style({
-  fontSize: theme.fontSize.xxlarge,
-  fontWeight: theme.fontWeight.bold,
+export const ul = style({
+  marginBottom: '20px',
+})
+
+globalStyle(`${ul} > li`, {
+  margin: `${theme.space.medium} 0`,
+  position: 'relative',
+  paddingInlineStart: theme.space.xlarge,
+  color: theme.color.bluegray,
+  fontSize: theme.fontSize.medium,
+})
+
+globalStyle(`${ul} > li:before`, {
+  content: '',
+  position: 'absolute',
+  top: '6px',
+  left: 0,
+  width: '6px',
+  height: '6px',
+  backgroundColor: theme.color.bluegray,
+  borderRadius: theme.borderRadius.circle,
 })
