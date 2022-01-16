a65badb232c1d6e9baf4148c3be0646084fc4e5f
tigger <fronttigger@gmail.com>
2022-01-17 01:55:24 +0900

    feat: 어바웃 타이틀 및 경력 섹션 마크업 구현 중

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,46 @@
+import config from 'config'
+
 import * as styles from './styles.css'
 
 import PageSEO from '#components/shared/SEO/PageSEO'
+import SocialIcon from '#components/icons'
 
 function About() {
   return (
     <>
       <PageSEO title="About" url="https://tigger.dev/about" />
-      <section className={styles.container}>About</section>
+      <section className={styles.container}>
+        <article className={styles.articleContainer}>
+          <h1 className={styles.title}>{config.about.title}</h1>
+          <p className={styles.description}>{config.about.description}</p>
+          <SocialIcon kind="github" href={config.author.contacts.github} />
+          <SocialIcon kind="linkedin" href={config.author.contacts.linkedin} />
+        </article>
+        <article className={styles.articleContainer}>
+          <h2 className={styles.subtitle}>경력 👨🏻‍💻</h2>
+          <div className={styles.experienceContainer}>
+            <div className={styles.companyContainer}>
+              <h3 className={styles.companyTitle}>프립(프렌트립)</h3>
+              <span className={styles.companySubtitle}>Frontend Engineer</span>
+              <span className={styles.companySubtitle}>2021. 06 ~ 현재</span>
+            </div>
+            <div className={styles.companyDescription}>
+              <div>
+                <h4>프립 웹 서비스 개편</h4>
+                <span>2021. 07 ~ 2022. 01</span>
+                <div>
+                  <strong>프립 웹 서비스와 관련된 개발 진행</strong>
+                  <ul>
+                    <li>ContextAPI와 Hook을 이용한 상태관리</li>
+                    <li>Redux와 Redux-saga를 활용한 글로벌 상태관리 및 비동기 처리</li>
+                    <li>REST API에서 GraphQL Apollo로 전환</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+      </section>
     </>
   )
 }
--- src/pages/about/styles.css.ts
+++ src/pages/about/styles.css.ts
@@ -1,6 +1,136 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
 
-// import { theme } from '#components/shared/theme.css'
-// import { sprinkles } from '#components/shared/sprinkles.css'
+import { theme } from '#components/shared/theme.css'
+import { mediaQuery, sprinkles } from '#components/shared/sprinkles.css'
 
-export const container = style([])
+export const container = style([
+  sprinkles({
+    marginY: {
+      desktop: 'xgreat',
+      tablet: 'xgreat',
+      mobile: 'xxxlarge',
+    },
+  }),
+])
+
+export const articleContainer = style({
+  marginBottom: theme.space.xgreat,
+})
+
+export const title = style([
+  sprinkles({
+    fontSize: {
+      desktop: 'xxlarge',
+      tablet: 'xlarge',
+      mobile: 'xlarge',
+    },
+    marginBottom: {
+      desktop: 'xxxlarge',
+      tablet: 'xxxlarge',
+      mobile: 'xxlarge',
+    },
+  }),
+  {
+    color: theme.color.darkblue,
+    fontWeight: theme.fontWeight.bold,
+    lineHeight: '1.2',
+    whiteSpace: 'pre-line',
+  },
+])
+
+export const description = style([
+  sprinkles({
+    fontSize: {
+      desktop: 'large',
+      tablet: 'large',
+      mobile: 'medium',
+    },
+  }),
+  {
+    marginBottom: theme.space.large,
+    lineHeight: '1.3',
+    whiteSpace: 'pre-line',
+  },
+])
+
+export const subtitle = style([
+  sprinkles({
+    fontSize: {
+      desktop: 'xlarge',
+      tablet: 'xlarge',
+      mobile: 'large',
+    },
+    marginBottom: {
+      desktop: 'xxlarge',
+      tablet: 'xxlarge',
+      mobile: 'xlarge',
+    },
+  }),
+  {
+    color: theme.color.darkblue,
+    fontWeight: theme.fontWeight.bold,
+  },
+])
+
+export const experienceContainer = style([
+  sprinkles({
+    flexDirection: {
+      desktop: 'row',
+      tablet: 'column',
+      mobile: 'column',
+    },
+    justifyContent: {
+      desktop: 'space-between',
+      tablet: 'flex-start',
+      mobile: 'flex-start',
+    },
+  }),
+  {
+    display: 'flex',
+  },
+])
+
+export const companyContainer = style([
+  sprinkles({
+    marginBottom: {
+      desktop: 'none',
+      tablet: 'xxlarge',
+      mobile: 'xxlarge',
+    },
+  }),
+])
+
+export const companyTitle = style({
+  marginBottom: theme.space.medium,
+  fontSize: theme.fontSize.large,
+})
+
+export const companySubtitle = style({
+  display: 'block',
+  marginBottom: theme.space.small,
+  fontSize: theme.fontSize.medium,
+})
+
+export const companyDescription = style({
+  '@media': {
+    [mediaQuery.mobile]: {
+      selectors: {
+        '&:last-of-type': {
+          paddingLeft: theme.space.large,
+          borderLeft: `${theme.space.small} solid ${theme.color.skyblue}`,
+        },
+      },
+    },
+    [mediaQuery.tablet]: {
+      selectors: {
+        '&:last-of-type': {
+          paddingLeft: theme.space.large,
+        },
+      },
+    },
+  },
+})
+
+globalStyle(`${container} > *`, {
+  color: theme.color.bluegray,
+})
