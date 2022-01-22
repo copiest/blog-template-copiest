e93c064fbfce7f59c21a6e8bb2ca15474578995d
tigger <fronttigger@gmail.com>
2022-01-23 01:39:53 +0900

    fix: 공통 이미지 컴포넌트 기본 100% 가지고 있도록 수정

--- src/components/shared/MDX/Image/index.tsx
+++ src/components/shared/MDX/Image/index.tsx
@@ -3,8 +3,8 @@ import * as styles from './styles.css'
 const Image = ({
   src,
   alt,
-  width,
-  height,
+  width = '100%',
+  height = '100%',
 }: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>) => (
   <img className={styles.image} src={src || ''} alt={alt} width={width} height={height} />
 )
